d1273ada34ab4ddf8d44695b8476867602218e69
Viktor Kukurba <viktor.kukurba@gmail.com>
2018-10-28 11:52:16 +0200

    Update styles

--- src/app/directives/img-loader.directive.ts
+++ src/app/directives/img-loader.directive.ts
@@ -1,17 +1,27 @@
 import { Directive, OnInit, Input, ElementRef } from '@angular/core';
 
+const DEFAULT_IMG =  '../../../assets/images/lidikart.jpg';
+
 @Directive({
-  selector: '[img-loader]'
+  selector: '[img-loader]',
+  host: {
+    '[style.background-size]': '"cover"',
+    '[style.background-repeat]': '"no-repeat"',
+    '[style.background-position]': '"50%"'
+  }
 })
 export class ImgLoaderDirective implements OnInit {
   @Input('img-loader')
   url: string;
-  constructor(private element: ElementRef) {}
+  private loaded: Boolean = false;
+  constructor(private element: ElementRef) {
+    this.setImage();
+  }
 
-  private setBackgroung() {
-    const element: HTMLElement = this.element.nativeElement;
-    element.style.backgroundImage = `url(${this.url})`;
-    element.classList.add('loaded');
+  private setImage() {
+    const element = this.element.nativeElement;
+    element.style.backgroundImage = this.loaded ? `url(${this.url})` : `url(${DEFAULT_IMG})`;
+    this.loaded && element.classList.add('loaded');
   }
 
   ngOnInit() {
@@ -19,10 +29,12 @@ export class ImgLoaderDirective implements OnInit {
     img.src = this.url;
     img.style.display = 'none';
     if (img.complete) {
-      this.setBackgroung();
+      this.loaded = true;
+      this.setImage();
     } else {
       img.onload = () => {
-        this.setBackgroung();
+        this.loaded = true;
+        this.setImage();
       };
     }
     document.body.appendChild(img);
